f49e78b34629f4d5db1c428bb47602e220a98b5f
Rodion Pushkarskiy <pushkarskiyrodion@gmail.com>
2023-06-11 19:15:55 +0300

    fix fetch request

--- src/api/CountriesAndCities.js
+++ src/api/CountriesAndCities.js
@@ -2,6 +2,8 @@ import axios from "axios";
 
 const BASE_URL = process.env.REACT_APP_BASE_URL;
 
+console.log(BASE_URL + "/countries/cities")
+
 export const getCities = async (selectedCounty) => {
   return await axios
     .post(BASE_URL + "/countries/cities", {
